a2501754a678503b3afc3ab2e9779a10c7aaf520
ezeswci <ezequiel@swci.com.ar>
2018-05-05 19:53:03 -0300

    mqtt service

---
+++ iot-mqtt/server.js
@@ -0,0 +1,136 @@
+'use strict'
+
+const debug = require('debug')('nodejs-test-iot:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+const db = require('iot-db')
+
+const { parsePayload } = require('./utils')
+
+const backend = {
+  type: 'redis',
+  redis,
+  return_buffers: true
+}
+
+const settings = {
+  port: 1883,
+  backend
+}
+
+const config = {
+  database: process.env.DB_NAME || 'iottest',
+  username: process.env.DB_USER || 'iot',
+  password: process.env.DB_PASS || 'iot',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5433,
+  operatorsAliases: false, // Recomendation from website
+  dialect: 'postgres',
+  logging: s => debug(s)
+}
+
+const server = new mosca.Server(settings)
+const clients = new Map()
+
+let Agent, Metric
+
+server.on('clientConnected', client => {
+  debug(`Client Connected ${client.id}`)
+  clients.set(client.id, null)
+})
+
+server.on('clientDisconnected', async (client) => {
+  debug(`Client Disconnected ${client.id}`)
+  const agent = clients.get(client.id)
+  if (agent) {
+    // Mark Agent as Disconnected
+    agent.connected = false
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (e) {
+      handleError(e)
+    }
+    // Delete Agent from list of clients
+    clients.delete(client.id)
+    server.publish({
+      topic: 'agent/disconnected',
+      payload: JSON.stringify({
+        uuid: agent.uuid
+      })
+    })
+    debug(`Client (${client.id}) associated to Agent (${agent.uuid}) marked as disconnected`)
+  }
+})
+
+server.on('published', async (packet, client) => {
+  debug(`Recived: ${packet.topic}`)
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+      const payload = parsePayload(packet.payload)
+      if (payload) {
+        payload.agent.connected = true
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+        debug(`Agent ${agent.uuid} saved`)
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+        // store Metrics
+        for (let metric of payload.metrics) { // For of suports asyncawait
+          let m
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (e) {
+            return handleError(e)
+          }
+          debug(`Metric ${m.id} saved on agent ${agent.uuid}`)
+        }
+      }
+      break
+  }
+})
+
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
+  console.log(`${chalk.green('[iot-mqtt]')} server is running`)
+})
+
+server.on('error', handleFatalError)
+process.on('uncaughtException', handleFatalError)
+process.on('unhandleRejection', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[error]')} ${err.message}`)
+  console.error(err.stack)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
